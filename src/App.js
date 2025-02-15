15a23ab1e6e827626e943568c7df85c74ed78289
sf-95 <sfarch95@gmail.com>
2025-02-15 21:14:48 +0000

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,18 +4,20 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Sarah Farchoukh and is
-        <a
-          href="https://github.com/sf-95/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {" "}
-          open-sourced on GitHub.
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Sarah Farchoukh and is
+          <a
+            href="https://github.com/sf-95/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            open-sourced on GitHub.
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
